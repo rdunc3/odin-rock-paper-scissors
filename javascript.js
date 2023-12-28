20aeff299198faf1882956e778e175d8d9ecef2f
Robert Duncan <robertdthree@gmail.com>
2023-12-28 14:59:39 -0800

    Very basic ui complete

--- javascript.js
+++ javascript.js
@@ -1,35 +1,67 @@
-function getComputerChoice(){
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    let choice = choices[Math.floor(Math.random()*choices.length)]
-    console.log('Computer chooses ' + choice);
-    return choice;
-}
-
-function playRound(playerSelection, computerSelection){
-    let p = playerSelection.toLowerCase();
-    let c = computerSelection.toLowerCase();
-    let result = "";
-    if(p == 'rock'){
-        if(c == 'paper')result = "You Lose! Paper beats Rock";
-        else result = "You Win! Rock beats Scissor";
-    }
-    else if(p == 'paper'){
-        if(c == 'scissor')result = "You Lose! Scissor beats Paper";
-        else result = "You Win! Paper beats Rock";
-    }
-    else if(p == 'scissor'){
-        if(c == 'rock')result = "You Lose! Rock beats Scissor";
-        else result = "You Win! Scissor beats Rock";
-    }
-    console.log(result);
-    return result;
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-    var choices = document.querySelectorAll('#choices button');
+    const choices = document.querySelectorAll('#choices button');
+    const playerScoreboard = document.querySelector('#playerScore');
+    const computerScoreboard = document.querySelector('#computerScore');
+    const winner = document.querySelector('#winner');
+    let playerScore = 0, computerScore = 0;
+
     choices.forEach(function(button) {
         button.addEventListener('click', function() {
-            playRound(button.id, getComputerChoice());
+            alert(playRound(button.id, getComputerChoice()));
         });
     });
+
+    function getComputerChoice(){
+        let choices = ['Rock', 'Paper', 'Scissors'];
+        let choice = choices[Math.floor(Math.random()*choices.length)]
+        //console.log('Computer chooses ' + choice);
+        alert('Computer chooses ' + choice);
+        return choice;
+    }
+    
+    function playRound(playerSelection, computerSelection){
+        let p = playerSelection.toLowerCase();
+        let c = computerSelection.toLowerCase();
+        let result = "";
+        if(p == c) return "DRAW";
+        if(p == 'rock'){
+            if(c == 'paper'){
+                result = "You Lose! Paper beats Rock";
+                computerScore++;
+            }
+            else {
+                result = "You Win! Rock beats Scissors";
+                playerScore++;
+            }
+        }
+        else if(p == 'paper'){
+            if(c == 'scissors'){
+                result = "You Lose! Scissors beats Paper";
+                computerScore++;
+            }
+            else {
+                result = "You Win! Paper beats Rock";
+                playerScore++;
+            }
+        }
+        else if(p == 'scissors'){
+            if(c == 'rock'){
+                result = "You Lose! Rock beats Scissors";
+                computerScore++;
+            }
+            else {
+                result = "You Win! Scissors beats Paper";
+                playerScore++;
+            }
+        }
+        //console.log(result);
+    
+        playerScoreboard.textContent = playerScore;
+        computerScoreboard.textContent = computerScore;
+        
+        if(playerScore == 5)winner.textContent = "You Win!"
+        else if(computerScore == 5)winner.textContent = "You Lose."
+        
+        return result;
+    }
 });
