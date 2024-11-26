601f11741fdf5c405c03da3f2fc56bd9faafa700
rdunc3 <robertdthree@gmail.com>
2024-11-26 08:16:23 -0800

    human can choose choice

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) //Returns a 0, 1, or 2
-    if (choice == 0) return "Rock";
-    else if (choice == 1)return "Paper";
-    return "Scissors"
+    if (choice == 0) return "rock";
+    else if (choice == 1)return "paper";
+    return "scissors"
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let choice = prompt("Input rock, paper, or scissors");
+    return choice.toLowerCase();
+}
+
+// console.log(getHumanChoice());
